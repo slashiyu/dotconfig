83690cf4e4e309c2d6793d08ffc81c9998ae68e7
slashiyu <slashiyu@example.com>
2023-06-13 15:18:47 +0900

    Bugfix mdlink fish funxion.

--- fish/functions/mdlink.fish
+++ fish/functions/mdlink.fish
@@ -2,7 +2,7 @@ function mdlink
     set -f title (grep "^title:" $argv | head -n 1 | sed -e 's/^title:\(.*\)$/\1/')
 
     if ! string length -q -- $title
-        set -f title (grep "^# :" $argv | head -n 1 | sed -e 's/^# \(.*\)$/\1/')
+        set -f title (grep "^# " $argv | head -n 1 | sed -e 's/^# \(.*\)$/\1/')
     end
 
     if ! string length -q -- $title
