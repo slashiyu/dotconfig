7d4d9abae48635b9b45b8d59ed85636d0dcb9355
slashiyu <slashiyu@example.com>
2023-10-03 18:33:22 +0900

    Deal with spacemacs gpg error.

--- installer/msys2-add-software.fish
+++ installer/msys2-add-software.fish
@@ -1,5 +1,6 @@
 #!/usr/bin/fish
 
+## Install spacemacs 
 pacman -S mingw-w64-ucrt-x86_64-adobe-source-code-pro-fonts
 
 pacman -S mingw-w64-ucrt-x86_64-emacs
@@ -7,5 +8,8 @@ git clone -b develop https://github.com/syl20bnr/spacemacs ~/.config/emacs
 
 setx SPACEMACSDIR "$XDG_CONFIG_HOME/spacemacs"
 setx GNUPGHOME "$HOME/.gnupg"
+
+## about this command :  https://datsuka-qwerty.hatenablog.com/entry/emacs/installspacemacs
+gpg --homedir ~/.config/emacs/elpa/gnupg --keyserver hkp://keys.gnupg.net --recv-keys 066DAFCB81E42C40
 
 emacs &
