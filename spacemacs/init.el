0700817a596086d895d3192218a3266820013a62
slashiyu <slashiyu@example.com>
2023-11-09 12:39:44 +0900

    Change theme of spacemacs.

--- spacemacs/init.el
+++ spacemacs/init.el
@@ -60,7 +60,7 @@ This function should only modify configuration layer settings."
      ;; spell-checking
      ;; syntax-checking
      ;; version-control
-     themes-megapack
+     ;; themes-megapack
      treemacs)
 
 
@@ -250,9 +250,7 @@ It should only modify the values of Spacemacs settings."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press `SPC T n' to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(zonokai-red
-                         spacemacs-dark
-                         spacemacs-light)
+   dotspacemacs-themes '(spacemacs-light)
 
    ;; Set the theme for the Spaceline. Supported themes are `spacemacs',
    ;; `all-the-icons', `custom', `doom', `vim-powerline' and `vanilla'. The
