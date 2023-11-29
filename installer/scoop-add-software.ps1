c194af4792bc176af156dbdbf92696782a723cb1
slashiyu <slashiyu@example.com>
2023-11-30 02:46:54 +0900

    Add babashka and clojure.

--- installer/scoop-add-software.ps1
+++ installer/scoop-add-software.ps1
@@ -3,6 +3,10 @@ scoop bucket add main
 scoop bucket add java
 scoop bucket add extras
 
+scoop bucket add scoop-clojure https://github.com/littleli/scoop-clojure
+
 scoop install main/git
 scoop install java/temurin-jdk
+scoop install scoop-clojure/babashka
+scoop install scoop-clojure/clj-deps
 scoop install extras/plantuml
