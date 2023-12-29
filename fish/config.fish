104c73a5283635a9e312b516954572208a519de0
slashiyu <slashiyu@example.com>
2023-12-29 15:45:22 +0900

    Add fish functions in leverage dir.

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,11 @@
+set -l -a __func_dirs ~/leverage/config/fish/functions
+
+for __func_dir in $__func_dirs
+    if test -d $__func_dir; and not contains $__func_dir $fish_function_path
+        set -p fish_function_path $__func_dir
+    end
+end
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
     fish_add_path -g /opt/bin
