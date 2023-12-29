83bdde6fc2c3b6227d72f0ad4a4deaf842951199
slashiyu <slashiyu@example.com>
2023-12-29 20:15:45 +0900

    Merged msys2-add-software.fish and scoop-add-software.ps1 into a single script.

--- installer/add-software.fish
+++ installer/add-software.fish
@@ -1,5 +1,17 @@
 #!/usr/bin/fish
 
+scoop bucket add main
+scoop bucket add java
+scoop bucket add extras
+
+scoop bucket add scoop-clojure https://github.com/littleli/scoop-clojure
+
+scoop install main/git
+scoop install java/temurin-jdk
+scoop install scoop-clojure/babashka
+scoop install scoop-clojure/clj-deps
+scoop install extras/plantuml
+
 ## Install spacemacs 
 pacman -S mingw-w64-ucrt-x86_64-adobe-source-code-pro-fonts
 
