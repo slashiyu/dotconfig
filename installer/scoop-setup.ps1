304591b4f7c93afc7b6a72a99ebbd8f7ba15ec04
slashiyu <slashiyu@example.com>
2024-07-19 14:26:06 +0900

    Update scoop dirs.

--- installer/scoop-setup.ps1
+++ installer/scoop-setup.ps1
@@ -1,9 +1,9 @@
 
-$working_dir = "~/tmp"
-$scoop_top_dir = "C:/msys64/opt/scoop"
-$scoop_dir = $scoop_top_dir + "/root"
-$scoop_global_dir = $scoop_top_dir + "/global"
-$scoop_cache_dir = $scoop_top_dir + "/cache"
+$working_dir = "~\tmp"
+$scoop_top_dir = "C:\scoop"
+$scoop_dir = $scoop_top_dir
+$scoop_global_dir = $scoop_top_dir + "\global"
+$scoop_cache_dir = $scoop_top_dir + "\cache"
 
 cd $working_dir
 irm get.scoop.sh -outfile 'install.ps1'
