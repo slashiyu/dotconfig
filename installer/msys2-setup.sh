4defc97a443d8dc482914cd96be25ec2116eb374
slashiyu <slashiyu@example.com>
2024-10-21 16:32:05 +0900

    Update installer/msys2-setup.sh.

--- installer/msys2-setup.sh
+++ installer/msys2-setup.sh
@@ -4,22 +4,23 @@ TIMESTAMP=`date +%Y%m%d-%H%M%S.%N`
 
 pacman -S fish
 
-CONFIG_DIR="/c/Users/${USER}/.config"
+CONFIG_DIR="/cygdrive/c/Users/${USER}/.config"
+MSYS_DIR="/cygdrive/c/msys64"
 
-cp /c/msys64/ucrt64.ini /c/msys64/ucrt64.ini.${TIMESTAMP}
-cp ${CONFIG_DIR}/msys2/ucrt64.ini /c/msys64/ucrt64.ini
+cp ${MSYS_DIR}/ucrt64.ini ${MSYS_DIR}/ucrt64.ini.${TIMESTAMP}
+cp ${CONFIG_DIR}/msys2/ucrt64.ini ${MSYS_DIR}/ucrt64.ini
 
-cp /c/msys64/etc/fstab /c/msys64/etc/fstab.${TIMESTAMP}
-cp ${CONFIG_DIR}/msys2/etc/fstab /c/msys64/etc/fstab
+cp ${MSYS_DIR}/etc/fstab ${MSYS_DIR}/etc/fstab.${TIMESTAMP}
+cp ${CONFIG_DIR}/msys2/etc/fstab ${MSYS_DIR}/etc/fstab
 
 USER_HOME="c:/Users/${USER}"
 
-setx HOME "${USER_HOME}"
-setx XDG_CONFIG_HOME "${USER_HOME}/.config"
-setx XDG_CACHE_HOME "${USER_HOME}/.cache"
-setx XDG_DATA_HOME "${USER_HOME}/.local/share"
-setx XDG_STATE_HOME "${USER_HOME}/.local/state"
+setx HOME $(cygpath -w "${USER_HOME}")
+setx XDG_CONFIG_HOME $(cygpath -w "${USER_HOME}/.config")
+setx XDG_CACHE_HOME $(cygpath -w "${USER_HOME}/.cache")
+setx XDG_DATA_HOME $(cygpath -w "${USER_HOME}/.local/share")
+setx XDG_STATE_HOME $(cygpath -w "${USER_HOME}/.local/state")
 
-setx SHELL "c:/msys64/usr/bin/fish"
+setx SHELL $(cygpath -w "c:/msys64/usr/bin/fish")
 
 echo "Restart terminal!"
