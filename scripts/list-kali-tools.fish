caf7381ff4eb995aacc57a2dcbd88b3c0a2cffff
slashiyu <slashiyu@example.com>
2024-12-18 17:24:12 +0900

    scripts: Add list-kali-tools.fish.

---
+++ scripts/list-kali-tools.fish
@@ -0,0 +1,18 @@
+#!/usr/bin/env fish
+
+set -l help_messages "
+This script generates a list of tool names and links to their descriptions based on the content of the URL: https://www.kali.org/tools/ .
+
+Usage:
+curl -L https://www.kali.org/tools/ | list-kali-tools
+"
+
+argparse -n list-kali-tools 'h/help' -- $argv
+or return 1
+
+if set -lq _flag_help
+  echo $help_messages
+  return
+end
+
+sed -z -e 's/\n//g' |sed -e 's@<div class=card>@\n\0@g' -e 's@</li></ul></div>@\0\n@g' |grep '<div class=card>' | sed -e 's@<div class=card><ul><li><a href=\([^"]*\)><i class=ti-archive [^>]*></i>\([^<]*\)<.*$@\2 \1@g'
